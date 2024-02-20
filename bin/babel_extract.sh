4647b95c03afc8b3fe2a759bc2816c21c3a10c3c
mcdogg17 <makdog45@mail.ru>
2024-02-20 15:36:53 +0500

    update ru translate

--- bin/babel_extract.sh
+++ bin/babel_extract.sh
@@ -1,5 +1,3 @@
 pybabel extract -F ./babel/babel.cfg -k lazy_gettext -o ./babel/messages.pot .
-pybabel update -i ./babel/messages.pot -d app/translations
-pybabel compile -d app/translations
-
-
+pybabel update -i ./babel/messages.pot -d flask_appbuilder/translations
+pybabel compile -d flask_appbuilder/translations
